0d9e20cdbac7c29466c3be0bd8cd8eed91c005eb
Yauri Attamimi <yaurigneel@gmail.com>
2024-08-18 21:50:21 +0700

    refactor: initiate traffic counter app

--- src/main/java/link/yauritux/TrafficCounterMainStarter.java
+++ src/main/java/link/yauritux/TrafficCounterMainStarter.java
@@ -1,13 +1,23 @@
 package link.yauritux;
 
+import link.yauritux.adapter.input.TrafficRecordServiceImpl;
+import link.yauritux.adapter.output.FileTrafficRecordRepository;
+import link.yauritux.application.TrafficCounterApplication;
+import link.yauritux.domain.repository.TrafficRecordRepository;
+import link.yauritux.domain.service.TrafficRecordService;
+
 /**
  * @author Yauri Attamimi (yaurigneel@gmail.com)
  * @version 1.0
  *
  */
 public class TrafficCounterMainStarter {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main( String[] args ) {
+        TrafficRecordRepository repository = new FileTrafficRecordRepository("traffic_data.txt");
+        TrafficRecordService service = new TrafficRecordServiceImpl(repository);
+        TrafficCounterApplication application = new TrafficCounterApplication(service);
+
+        application.run();
     }
 }
